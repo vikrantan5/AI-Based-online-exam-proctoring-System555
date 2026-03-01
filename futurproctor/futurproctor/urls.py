206e663f2f842b98f91ae3b3ab4820ff62d7a86b
Vikrant Singh <vikrantsinghan5@gmail.com>
2026-03-01 13:18:09 +0530

    fgh

--- futurproctor/futurproctor/urls.py
+++ futurproctor/futurproctor/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
-    path('admin/', admin.site.urls),
+    # Important: Proctoring URLs must come BEFORE admin URLs
+    # to allow custom admin views (admin/dashboard-enhanced/) to work
     path('', include('proctoring.urls')),  # Include your app's URLs
+    path('admin/', admin.site.urls),  # Django admin comes after
     
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
