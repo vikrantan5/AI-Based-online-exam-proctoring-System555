fab529d8ea4a4d4a4139a2cd734cd8b968805130
Vikrant Singh <vikrantsinghan5@gmail.com>
2026-03-07 11:43:57 +0530

    student_photos

--- futurproctor/futurproctor/settings.py
+++ futurproctor/futurproctor/settings.py
@@ -142,7 +142,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static'] if os.path.exists(BASE_DIR / 'static') else []
 
-
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
